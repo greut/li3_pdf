b4555fcccd2cf43d59d83321b7af3cc68ac10dad
Yoan Blanc <yoan@dosimple.ch>
2012-03-19 12:10:13 +0100

    Do not load DOMPDF if not there.

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -6,22 +6,22 @@ if (!defined('DOMPDF_LIBRARY_PATH')) {
 	define('DOMPDF_LIBRARY_PATH', LITHIUM_LIBRARY_PATH . '/dompdf/dompdf');
 }
 
-define('DOMPDF_INC_DIR', DOMPDF_LIBRARY_PATH . '/include', true);
-define('DOMPDF_LIB_DIR', DOMPDF_LIBRARY_PATH . '/lib', true);
+if (file_exists(DOMPDF_LIBRARY_PATH)) {
+	define('DOMPDF_INC_DIR', DOMPDF_LIBRARY_PATH . '/include', true);
+	define('DOMPDF_LIB_DIR', DOMPDF_LIBRARY_PATH . '/lib', true);
 
-require DOMPDF_INC_DIR . '/functions.inc.php';
+	require DOMPDF_INC_DIR . '/functions.inc.php';
 
+	$name = 'dompdf';
+	$library = Libraries::get($name);
 
-$name = 'dompdf';
-$library = Libraries::get($name);
-
-if (empty($library)) {
-	Libraries::add($name, array(
-		'bootstrap' => false,
-		'path' => DOMPDF_LIBRARY_PATH
-	));
+	if (empty($library)) {
+		Libraries::add($name, array(
+			'bootstrap' => false,
+			'path' => DOMPDF_LIBRARY_PATH
+		));
+	}
 }
-
 
 /**
  * Configure media to output PDF
